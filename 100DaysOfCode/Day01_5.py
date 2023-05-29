7d3d23b459aef00ea881b5b8369c737684a59a45
Dineshkumar Subramanian <todineshkumar@outlook.com>
2023-05-29 20:11:41 +0530

    Added Complete Day01 of 100DaysOfCode

---
+++ 100DaysOfCode/Day01_5.py
@@ -0,0 +1,2 @@
+#using input function to print hello and the input i enter
+print("hello"+" "+ input("What's your name:")+"!")
---
+++ 100DaysOfCode/Day01_final.py
@@ -0,0 +1,15 @@
+# Create a greeting for your program.
+
+print('"welcome to the Brand Name Generator" \n')
+
+# ask the user for the city that they grew Un
+city_name = input("your city name? \n")
+
+# ask tthe user for the name of pet 
+pet_name = input("your pet name? \n")
+
+# combine the name of their city and pet and show their band Name
+brand_name= city_name +" " + pet_name;
+print("your brand name can be : " + brand_name)
+
+# make sure the input cuursor shows on a new line.
