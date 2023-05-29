37da99303a55873d9a8c7c61466dca52acdbdfa1
Dineshkumar Subramanian <todineshkumar@outlook.com>
2023-05-29 20:07:44 +0530

    added day01 of 100DaysOfCode

---
+++ 100DaysOfCode/Day01_1.py
@@ -0,0 +1,5 @@
+#print("Day 1 - Python print Function")
+#print("The function is declared like this:")
+#print("print ('what to print')")
+
+print("Day 1 - Python print Function" + "\n" +"The function is declared like this:" + "\n" + "print ('what to print')")
---
+++ 100DaysOfCode/Day01_3.py
@@ -0,0 +1,7 @@
+# Write a program that prints the number of characters in a user's name.
+
+x = input("your name:")
+print(len(x))
+
+# or
+print(len(input("your name:")))
