6d9d799facd04f0cea0400e3731c6e0ba372ccaf
Dineshkumar Subramanian <todineshkumar@outlook.com>
2023-07-11 23:31:09 +0530

    updated some code base of coffe machine

--- 100DaysOfCode/Day16/main.py
+++ 100DaysOfCode/Day16/main.py
@@ -2,8 +2,17 @@ from menu import Menu, MenuItem
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
 
+
+#machine = "on"
+#while machine=="on":
+#    pass
 menu=Menu()
 #print(menu.get_items())
 
 user_input=input(f"what would you like? {menu.get_items()} \n")
-item=menu.find_drink(user_input)
+if user_input=="report":
+    CoffeeMaker().report()
+else:
+    item=menu.find_drink(user_input)
+    if CoffeeMaker().is_resource_sufficient(item):
+        CoffeeMaker().make_coffee(item)
